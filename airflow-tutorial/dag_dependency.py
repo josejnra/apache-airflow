ed36c53ecb6aae4615ba4ee2568765086b0558a1
José Nunes <josenunesra@gmail.com>
2021-01-17 19:55:28 -0300

    cross-dag dependency

--- airflow-tutorial/dag_dependency.py
+++ airflow-tutorial/dag_dependency.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 from airflow import DAG
-from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.sensors.external_task import ExternalTaskSensor, ExternalTaskMarker
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
@@ -21,7 +21,14 @@ with DAG(dag_id=set_dag_id(__file__) + '-parent', start_date=days_ago(1), schedu
         bash_command="sleep 10s"
     )
 
-    end = DummyOperator(task_id='end')
+    # Use Task Marker in case to clear child task if this task is cleared
+    end = ExternalTaskMarker(
+        task_id="end",
+        external_dag_id="dag-dependency-child",
+        external_task_id="child_task1",
+    )
+
+    start >> do_something >> end
 
 
 with DAG(dag_id=set_dag_id(__file__) + '-child', start_date=days_ago(1), schedule_interval="@daily") as child_dag:
