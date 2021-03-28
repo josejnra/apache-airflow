b89d9f3e89657f7222b5c0cb57a274501c618aac
José Nunes <josenunesra@gmail.com>
2021-03-28 12:36:43 -0300

    refactoring tasks with weight

--- airflow-tutorial/tasks_with_weight.py
+++ airflow-tutorial/tasks_with_weight.py
@@ -1,39 +1,35 @@
 import os
 import sys
 
-from airflow.operators.dummy import DummyOperator
-
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 
 from airflow_utils import set_dag_id
 
 
-with DAG(dag_id=set_dag_id(__file__), start_date=days_ago(1), schedule_interval=None) as dag:
+with DAG(dag_id=set_dag_id(__file__), start_date=days_ago(1), schedule_interval=None, concurrency=1) as dag:
 
     start = DummyOperator(task_id='start')
 
     task_1 = BashOperator(
-        task_id='task_1',
-        bash_command='echo task 1',
-        pool="pool_tasks_weight_1",
+        task_id='task_1_priority_1',
+        bash_command='sleep 5; echo task 1',
         priority_weight=1
     )
 
     task_2 = BashOperator(
-        task_id='task_2',
-        bash_command='echo task 2',
-        pool="pool_tasks_weight_2",
+        task_id='task_2_priority_2',
+        bash_command='sleep 5; echo task 2',
         priority_weight=2
     )
 
     task_3 = BashOperator(
-        task_id='task_3',
-        bash_command='echo task 3',
-        pool="pool_tasks_weight_2",
+        task_id='task_3_priority_2',
+        bash_command='sleep 5; echo task 3',
         priority_weight=2
     )
 
